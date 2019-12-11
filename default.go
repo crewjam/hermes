9ecbdbd99f5eb2de37d40016ce01e6017b258034
Ross Kinder <ross@kndr.org>
2019-12-11 11:46:12 -0500

    convert Intros and Outros to markdown

--- default.go
+++ default.go
@@ -302,7 +302,7 @@ func (dt *Default) HTMLTemplate() string {
                     {{ with .Email.Body.Intros }}
                         {{ if gt (len .) 0 }}
                           {{ range $line := . }}
-                            <p>{{ $line }}</p>
+                            <p>{{ $line.ToHTML }}</p>
                           {{ end }}
                         {{ end }}
                     {{ end }}
@@ -441,7 +441,7 @@ func (dt *Default) HTMLTemplate() string {
                     {{ with .Email.Body.Outros }} 
                         {{ if gt (len .) 0 }}
                           {{ range $line := . }}
-                            <p>{{ $line }}</p>
+                            <p>{{ $line.ToHTML }}</p>
                           {{ end }}
                         {{ end }}
                       {{ end }}
